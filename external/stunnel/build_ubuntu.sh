20fb6aaa16c8143c1c46e7e3bc86dd55277209bb
Richard Collins <richard.collins@bristol.ac.uk>
2018-11-02 12:57:32 +0000

    patch for stunnel to use psk

---
+++ external/stunnel/build_ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (C) University of Bristol 2018
+#    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+#    If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#    See LICENSE file for details.
+# Author Richard Collins <richard.collins@bristol.ac.uk>
+#
+
+apt source stunnel4 && \
+cd `find -maxdepth 1 -type d -name 'stunnel4-*'` && \
+quilt import -P cqptoolkit-psk-deb.patch ../cqptoolkit-psk-deb.patch ; \
+dpkg-buildpackage --no-sign
+
