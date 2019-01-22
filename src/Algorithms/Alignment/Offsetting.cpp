2ec63bb47ed73f89a1f524e3c2614246933ae1f1
Richard Collins <richard.collins@bristol.ac.uk>
2019-01-22 09:15:29 +0000

    Added code for comparing offsets

--- src/Algorithms/Alignment/Offsetting.cpp
+++ src/Algorithms/Alignment/Offsetting.cpp
@@ -3,9 +3,57 @@
 namespace cqp {
     namespace align {
 
-        Offsetting::Offsetting()
+        Offsetting::Offsetting(size_t samples,
+                               double rejectionThreshold,
+                               double acceptanceThreshold,
+                               size_t minTests):
+            rejectionThreshold(rejectionThreshold),
+            acceptanceThreshold(acceptanceThreshold),
+            minTests(minTests),
+            samples(samples)
         {
 
+        }
+
+        double Offsetting::CompareValues(const QubitList& truth, const std::vector<uint64_t>& validSlots,
+                                         const QubitList& irregular,
+                                         ssize_t offset)
+        {
+            using namespace std;
+            double confidence = 0.5;
+            size_t basesMatched = 0;
+            size_t validCount = 0;
+
+            const auto smallestList = min(truth.size(), irregular.size());
+            const auto actualSamples = min(smallestList, samples);
+            const size_t step = smallestList / actualSamples;
+
+            const size_t indexEnd = static_cast<size_t>(
+                        max(0l, min(static_cast<ssize_t>(truth.size()), static_cast<ssize_t>(irregular.size()) - offset))
+                        );
+            const size_t indexStart = static_cast<size_t>(0 - min(0l, offset)); // offset the start if offset is negative
+
+            for(auto index = indexStart; index < indexEnd; index += step)
+            //for(auto slotId : validSlots)
+            {
+                const size_t irregIndex = index + offset;
+                if(QubitHelper::Base(truth[index]) == QubitHelper::Base(irregular[irregIndex]))
+                {
+                    basesMatched++;
+                    if(truth[index] == irregular[irregIndex])
+                    {
+                        validCount++;
+                    }
+
+                    confidence = static_cast<double>(validCount) / basesMatched;
+                    if(basesMatched > minTests && (confidence < rejectionThreshold || confidence > acceptanceThreshold))
+                    {
+                        break; // for
+                    }
+                }
+            }
+
+            return confidence;
         }
 
     } // namespace align
--- src/Algorithms/Alignment/Offsetting.h
+++ src/Algorithms/Alignment/Offsetting.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "Algorithms/Datatypes/Qubits.h"
 
 namespace cqp {
     namespace align {
@@ -7,7 +7,35 @@ namespace cqp {
         class Offsetting
         {
         public:
-            Offsetting();
+            static constexpr double DefaultRejection = 0.4;
+            static constexpr double DefaultAcceptance = 0.7;
+            static constexpr size_t DefaultMinTests = 100;
+            static constexpr size_t DefaultSamples = 1000;
+
+            /**
+             * @brief Offsetting
+             * @param rejectionThreshold Comparison consistentlently below this will be rejected
+             * @param minTests The minimum number of comparisons to make before rejecting/accepting
+             */
+            Offsetting(size_t samples = DefaultSamples,
+                       double rejectionThreshold = DefaultRejection,
+                       double acceptanceThreshold = DefaultAcceptance,
+                       size_t minTests = DefaultMinTests);
+
+            /**
+             * @brief CompareValues
+             * @param truth
+             * @param irregular
+             * @param offset Shift the irregular comparison index.
+             * @return The match confidence
+             */
+            double CompareValues(const QubitList& truth,  const std::vector<uint64_t>& validSlots,
+                                 const QubitList& irregular, ssize_t offset);
+        protected:
+            double rejectionThreshold;
+            double acceptanceThreshold;
+            size_t minTests;
+            size_t samples;
         };
 
     } // namespace align
