02e92a581d17c578098e6f7465599eeb3fa34a67
Richard Collins <richard.collins@bristol.ac.uk>
2019-03-08 08:49:23 +0000

    proposed interface changes for qkd device as a separate program

---
+++ src/KeyManagement/Net/RemoteQKDDevice.cpp
@@ -0,0 +1,21 @@
+/*!
+* @file
+* @brief RemoteQKDDevice
+*
+* @copyright Copyright (C) University of Bristol 2019
+*    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+*    If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*    See LICENSE file for details.
+* @date 27/2/2019
+* @author Richard Collins <richard.collins@bristol.ac.uk>
+*/
+#include "RemoteQKDDevice.h"
+
+namespace cqp {
+
+    RemoteQKDDevice::RemoteQKDDevice()
+    {
+
+    }
+
+} // namespace cqp
---
+++ src/KeyManagement/Net/RemoteQKDDevice.h
@@ -0,0 +1,25 @@
+/*!
+* @file
+* @brief RemoteQKDDevice
+*
+* @copyright Copyright (C) University of Bristol 2019
+*    This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+*    If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*    See LICENSE file for details.
+* @date 27/2/2019
+* @author Richard Collins <richard.collins@bristol.ac.uk>
+*/
+#pragma once
+
+
+namespace cqp {
+
+    class RemoteQKDDevice
+    {
+    public:
+        RemoteQKDDevice();
+    };
+
+} // namespace cqp
+
+
