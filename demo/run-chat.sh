9ca39cfd548711442fe15b1e1153ec51e76555da
Richard Collins <richard.collins@bristol.ac.uk>
2019-05-02 14:19:12 +0100

    demo file tweaks

--- demo/run-chat.sh
+++ demo/run-chat.sh
@@ -40,12 +40,13 @@ byobu split-window -h "${DRIVER}" -q -c qkd-alice.json
 byobu select-pane -D -t 0
 # bottom right
 byobu split-window -h "${DRIVER}" -q -c qkd-bob.json
-sleep 1
+sleep 2
 byobu new-window -n VPN "${QTUN}" -c VPN-b.json
 byobu split-window -v "${QTUN}" -c VPN-a.json -a
 byobu select-pane -t 0
-byobu split-window -h "nc -t localhost 9000"
-byobu select-pane -D -t 0
+sleep 1
 byobu split-window -h "nc -t localhost 9001"
+byobu select-pane -D -t 0
+byobu split-window -h "nc -t localhost 9000"
 
 byobu attach
--- demo/run-devices.sh
+++ demo/run-devices.sh
@@ -5,6 +5,7 @@ if [ "$1" == "" ]; then
 	SITEAGENT=`which SiteAgentRunner 2>/dev/null`
 	DRIVER=${BUILDDIR}/Drivers/DummyQKDDriver/DummyQKDDriver
 	QTUN=${BUILDDIR}/Tools/QTunnelServer/QTunnelServer
+        STATS=${BUILDDIR}/Tools/StatsDump/StatsDump
 
 	if [ "$SITEAGENT" == "" ]; then
 		SITEAGENT=${BUILDDIR}/Tools/SiteAgentRunner/SiteAgentRunner
@@ -15,12 +16,16 @@ if [ "$1" == "" ]; then
 	if [ "QTUN" == "" ]; then
 		QTUN=${BUILDDIR}/Tools/QTunnelServer/QTunnelServer
 	fi
+        if [ "STATS" == "" ]; then
+                STATS=${BUILDDIR}/Tools/StatsDump/StatsDump
+        fi
 else 
 	BUILDDIR="$1"
 
 	SITEAGENT=${BUILDDIR}/Tools/SiteAgentRunner/SiteAgentRunner
 	DRIVER=${BUILDDIR}/Drivers/DummyQKDDriver/DummyQKDDriver
 	QTUN=${BUILDDIR}/Tools/QTunnelServer/QTunnelServer
+        STATS=${BUILDDIR}/Tools/StatsDump/StatsDump
 fi
 
 SESSION=driverdemo
@@ -29,8 +34,12 @@ byobu kill-session -t ${SESSION}
 # left
 byobu new-session -d -s "${SESSION}" -c . "${DRIVER} -b -k localhost:7000"
 byobu rename-window Drivers
+
+byobu split-window -v "${STATS} -c localhost:7000"
+
+byobu select-pane -t 0
 # right
 byobu split-window -h "${DRIVER} -a -m localhost:7000" 
-byobu select-pane -t 0 
+byobu select-pane -t 0
 
 byobu attach
